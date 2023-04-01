ff092147fc468c379312ffe5e52dd47f959d7172
Sajeed Shaikh <saj33d739@gmail.com>
2023-04-01 15:45:02 +0530

    #1: Added Category Model Made Connection with the database using ConnectionString

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
